d2131731e118b1c34d0bbea5684c66167db86e51
Johnny Pillacela <johnnybelesaca@gmail.com>
2026-04-06 13:51:35 -0400

    add gradient variant

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -19,6 +19,7 @@ const badgeVariants = cva(
         ghost:
           "hover:bg-muted hover:text-muted-foreground dark:hover:bg-muted/50",
         link: "text-primary underline-offset-4 hover:underline",
+        gradient: "bg-primary-gradient text-white [a]:hover:opacity-90",
       },
     },
     defaultVariants: {
